5058f54c36c4ddc81543574c3642a82382903d97
Aleh Serhiyenia <sergienya.oleg@yandex.ru>
2019-02-11 00:16:50 +0300

    init

---
+++ precache-manifest.327b1c93bf1576737454803892b09d24.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c161500ed5a40c2c74ca",
+    "url": "/static/js/main.c161500e.chunk.js"
+  },
+  {
+    "revision": "3c897b2bf54819b95c16",
+    "url": "/static/js/1.3c897b2b.chunk.js"
+  },
+  {
+    "revision": "c161500ed5a40c2c74ca",
+    "url": "/static/css/main.1f94ad18.chunk.css"
+  },
+  {
+    "revision": "41a0a96e10270b2b9a5796039e379427",
+    "url": "/index.html"
+  }
+];
