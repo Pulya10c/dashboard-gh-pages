565d0022cf185c08bf267915a90a1a1d977d1c26
Aleh Serhiyenia <sergienya.oleg@yandex.ru>
2019-02-11 15:29:21 +0300

    fix mistake

---
+++ precache-manifest.66b934ce1701ad6475ba932577eefbaa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "6c9c0cc38e7ba7858ec8",
+    "url": "./static/js/main.6c9c0cc3.chunk.js"
+  },
+  {
+    "revision": "3c897b2bf54819b95c16",
+    "url": "./static/js/1.3c897b2b.chunk.js"
+  },
+  {
+    "revision": "6c9c0cc38e7ba7858ec8",
+    "url": "./static/css/main.90693708.chunk.css"
+  },
+  {
+    "revision": "3d19976fa90ca91dea65d93b4d1f970e",
+    "url": "./index.html"
+  }
+];
