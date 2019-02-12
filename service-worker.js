c0cbf395f88dd272118e043e8a17784fed7ecd1d
Aleh Serhiyenia <sergienya.oleg@yandex.ru>
2019-02-12 20:21:42 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/dashboard-gh-pagesgit%20/precache-manifest.a22121d5bef0c901cd3c2a163999d873.js"
+  "/dashboard-gh-pages/precache-manifest.22155ea0b23cde62e0ec799eac050c20.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/dashboard-gh-pagesgit%20/index.html", {
+workbox.routing.registerNavigationRoute("/dashboard-gh-pages/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
